c528858ca1a83656512beda72b1363cc47793b44
Florian Dufter <florian.dufter@iteratec.com>
2019-11-02 19:08:16 +0100

    *changed icon, *changed app id

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Sira App',
       theme: ThemeData(
         primaryColor: Color.fromRGBO(58, 66, 86, 1.0),
